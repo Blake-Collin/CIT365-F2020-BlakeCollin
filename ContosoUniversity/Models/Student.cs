12bda508570fd41b579e894bbeade8b8d229350d
Collin Blake <collinblake1@gmail.com>
2020-11-18 23:42:08 -0700

    Week 10 University

---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace ContosoUniversity.Models
+{
+    public class Student : Person
+    {
+
+        [Display(Name = "Enrollment Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime EnrollmentDate { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
